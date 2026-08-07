9d3ef4665fb2109afb2c81e59350a03848cfc4a7
Souradeep De <souradeepde20xi@gmail.com>
2026-08-07 20:39:00 +0530

    Create Day_03.sql
    Today I completed the INSERT ROWS(22:38-28:32) from 
    SQL Full Course for free 🐬 video from BROCODE youtube channel. Looking forward for DAY 4 :)

---
+++ Day_03.sql
@@ -0,0 +1,13 @@
+INSERT INTO footballers (club_name, player_name) VALUES ("Juventus", "Erling Haaland");
+
+SELECT * FROM footballers LIMIT 0, 1000;
+
+INSERT INTO footballers VALUES 
+(4, 'Juventus', 'Erling Haaland', 242054.21, '2023-01-20'),
+(18, 'Manchester City', 'Harry Kane', 315998.63, '2022-03-24');
+
+SELECT * FROM footballers LIMIT 0, 1000;
+
+INSERT INTO footballers (squad_number, club_name, player_name) VALUES (5, 'FC Barcelona', 'Rodri');
+
+SELECT * FROM footballers LIMIT 0, 1000;
